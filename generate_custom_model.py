052ebffe8f8f7ca99b02e2633d24097c0a617974
tqer39 <tqer39@gmail.com>
2024-11-21 00:56:57 +0900

    🔧 generate_custom_model.py の引数解析を改善し、--copy-source-raw-directory オプションのフォーマットを整えました。

--- generate_custom_model.py
+++ generate_custom_model.py
@@ -2,12 +2,17 @@ import scripts.create_and_copy_data as create_and_copy_data
 import sys
 import argparse
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="カスタムモデルを生成します。")
     parser.add_argument("arg", nargs="?", help="引数")
     parser.add_argument("--model-name", required=True, help="モデル名")
-    parser.add_argument("--copy-source-raw-directory", help="元になる音声ファイル（mp3, wav など）のパスを指定するディレクトリ")
+    parser.add_argument(
+        "--copy-source-raw-directory",
+        help="元になる音声ファイル（mp3, wav など）のパスを指定するディレクトリ",
+    )
     return parser
+
 
 if __name__ == "__main__":
     parser = parse_arguments()
@@ -15,7 +20,11 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit(1)
     args = parser.parse_args()
-    create_and_copy_data.main([
-        "--model-name", args.model_name,
-        "--copy-source-raw-directory", args.copy_source_raw_directory
-    ])
+    create_and_copy_data.main(
+        [
+            "--model-name",
+            args.model_name,
+            "--copy-source-raw-directory",
+            args.copy_source_raw_directory,
+        ]
+    )
