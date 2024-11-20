13fbd9c68a5a49ada1a45fbe8e9bbbdd905acadf
tqer39 <tqer39@gmail.com>
2024-11-21 00:42:32 +0900

    🔧 コマンドライン引数の解析を修正し、引数が指定されていない場合にヘルプメッセージを表示する機能を追加しました。

--- generate_custom_model.py
+++ generate_custom_model.py
@@ -2,12 +2,18 @@ import scripts.create_and_copy_data as create_and_copy_data
 import sys
 import argparse
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="カスタムモデルを生成します。")
-    parser.add_argument("arg", help="引数")
+    parser.add_argument("arg", nargs="?", help="引数")
     parser.add_argument("--model-name", required=True, help="モデル名")
-    return parser.parse_args()
+    return parser
+
 
 if __name__ == "__main__":
-    args = parse_arguments()
+    parser = parse_arguments()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    args = parser.parse_args()
     create_and_copy_data.main(["--model-name", args.model_name])
